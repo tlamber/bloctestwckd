0a4e24de93e68b934ea667e9f508d0131d1624f3
iMac <imac@iMacs-iMac.local>
2021-01-01 20:43:20 -0800

    first commit

---
+++ lib/counter_bloc.dart
@@ -0,0 +1,21 @@
+import 'dart:developer';
+
+import 'package:bloc/bloc.dart';
+
+enum CounterEvents { increment, decrement }
+
+class CounterBloc extends Bloc<CounterEvents, int> {
+  CounterBloc() : super(0);
+
+  @override
+  Stream<int> mapEventToState(CounterEvents event) async* {
+    switch (event) {
+      case CounterEvents.increment:
+        yield state + 1;
+        break;
+      case CounterEvents.decrement:
+        yield state - 1;
+        break;
+    }
+  }
+}
